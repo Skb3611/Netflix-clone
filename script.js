7742c9e2bdc744655066732d0db7b131055946ba
SKB <shubhambhilare4899223@gmail.com>
2024-01-18 01:35:08 +0530

    page made js left

---
+++ script.js
@@ -0,0 +1,7 @@
+const wrapper =document.getElementsByClassName("box");
+Array.from(wrapper).forEach(element => {
+    addEventListener("click",()=>{
+        const btn=element.target;
+        
+    })
+});
