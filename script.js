4ead7b6a530949ee27d11c2567b26087c7f843e7
SKB <shubhambhilare4899223@gmail.com>
2024-01-19 01:41:20 +0530

    js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const wrapper =document.getElementsByClassName("box");
-Array.from(wrapper).forEach((e) => {
-    e.addEventListener("click",()=>{
-        console.log(e.targrt)
+const ans =Array.from(document.getElementsByClassName("ans"));
+const sign =document.getElementsByClassName("Sign");
+for (let index = 0; index < wrapper.length; index++) {
+    wrapper[index].addEventListener("click",()=>{
+        ans[index].classList.toggle("active")
+        sign[index].classList.toggle("activate")
+        
     })
-});
+    
+}
